724ec345010f49679c1f3fb3bbcaf239eb1a7fe0
Roger Barker <roger@pandaswhocode.com>
2025-08-07 22:35:28 -0500

    chore: Update initial action
    Signed-off-by: Roger Barker <roger@pandaswhocode.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
 
 /**
  * The main function for the action.
@@ -8,18 +7,32 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const service: string = core.getInput('service')
+    const group: string = core.getInput('group')
+    const environment: string = core.getInput('environment')
+    const severity: string = core.getInput('severity')
+    const title: string = core.getInput('title')
+    const createAlert: boolean = core.getInput('create-alert') == 'true'
+
+    // The API key is secret and shall not be logged in any way.
+    // The API key shall be used during requests but never logged or stored.
+    const apiKey: string = core.getInput('api-key')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Title: ${title}`)
+    core.debug(`Severity: ${severity}`)
+    core.debug(`Service: ${service}`)
+    core.debug(`Group: ${group}`)
+    core.debug(`Environment: ${environment}`)
+    core.debug(`Create Alert: ${createAlert}`)
+    core.debug(`API Key Length: ${apiKey.length}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // Do things
+    alertId = createAlert ? 'fake-alert-id' : ''
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('incident-id', 'fake-incident-id')
+    core.setOutput('alert-id', alertId)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
